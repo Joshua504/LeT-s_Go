1cd0135dc493955d257073fd6c74e67c0e7e9b65
chuokoh <chukwuebukajosh8@gmail.com>
2026-04-15 16:57:43 +0100

    feat: adding routers and opening and listening to the server

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// * Define a home handler function which writes a byte slice containing
+// * "Hello from Snippetbox" as the response body
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
 
 func main() {
-	fmt.Println("Got Mitt Uns")
+	//* Use the http.NewServeMux() function to initialize a new servemux, then
+	//* register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	//* Register the two new handler functions and corresponding route patterns with
+	//* the servemux, in exactly the same way that we did before.
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	//* Print a log message to say that the server is starting.
+	log.Print("Starting server on :4000")
+
+	//* Use the http.ListenAndServe() function to start a new web server. We pass in
+	//* two parameters: the TCP network address to listen on (in this case ":4000")
+	//* and the servemux we just created. If http.ListenAndServe() returns an error
+	//* we use the log.Fatal() function to log the error message and exit. Note
+	//* that any error returned by http.ListenAndServe() is always non-nil.
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
