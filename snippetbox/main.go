7ab03625ccaac14cdab476e652926879b5afb638
chuokoh <chukwuebukajosh8@gmail.com>
2026-04-17 09:18:29 +0100

    II

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // * Define a home handler function which writes a byte slice containing
@@ -12,7 +14,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	//* Extract the value of the id wildcard from the request using r.PathValue()
+	//* and try to convert it to an integer using the strconv.Atoi() function. If
+	//* it can't be converted to an integer, or the value is less than 1, we
+	//* return a 404 page not found response.
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	//* Use the fmt.Sprintf() function to interpolate the id value with a
+	//* message, then write it as the HTTP response.
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -23,11 +38,11 @@ func main() {
 	//* Use the http.NewServeMux() function to initialize a new servemux, then
 	//* register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) //? Restrict this route to exact matches on / only.
 
 	//* Register the two new handler functions and corresponding route patterns with
 	//* the servemux, in exactly the same way that we did before.
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view/{id}", snippetView) //? Add the {id} wildcard segment
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	//* Print a log message to say that the server is starting.
