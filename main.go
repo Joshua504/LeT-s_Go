7ce41fea46cd0172a79670b6f2514eb4d952ed23
chuokoh <chukwuebukajosh8@gmail.com>
2026-04-28 16:45:39 +0100

    feat: template composition II

--- main.go
+++ main.go
@@ -91,6 +91,16 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /{$}", home) //? Restrict this route to exact matches on / only.
 
+	//! Create a file server which serves files out of the "./ui/static" directory.
+	//! Note that the path given to the http.Dir function is relative to the project
+	//! directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	//? Use the mux.Handle() function to register the file server as the handler for
+	//? all URL paths that start with "/static/". For matching paths, we strip the
+	//? "/static" prefix before the request reaches the file server.
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	//* Register the two new handler functions and corresponding route patterns with
 	//* the servemux, in exactly the same way that we did before.
 
